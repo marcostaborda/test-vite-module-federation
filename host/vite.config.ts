a742906d626bc53eef09155cf3db17dbace946bb
Marcos Taborda <marcos_taborda@sicredi.com.br>
2025-01-15 00:49:47 -0300

    feat: first commit

---
+++ host/vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig, PluginOption } from 'vite'
+import react from '@vitejs/plugin-react'
+import federation from "@originjs/vite-plugin-federation";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react() as PluginOption,
+    federation({
+      name: 'host-app',
+      remotes: {
+        news: 'http://localhost:5001/assets/remoteEntry.js',
+        ads: 'http://localhost:5002/assets/remoteEntry.js',
+        sponsors: 'http://localhost:5003/assets/remoteEntry.js',
+        weather: 'http://localhost:5004/assets/remoteEntry.js',
+      },
+      shared: ['react', 'react-dom'],
+    }) as PluginOption
+  ],
+  build: {
+    modulePreload: false,
+    target: 'esnext',
+    minify: false,
+    cssCodeSplit: false
+  }
+})
